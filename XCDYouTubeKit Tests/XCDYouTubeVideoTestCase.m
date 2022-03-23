ebad27fc1059dd3e9f0d078aa3fdc1fe525f2886
Gabe Guerrero <gabe.guerrero@washpost.com>
2022-03-23 18:10:43 -0400

    IOS-5281: Removes response related tests

--- XCDYouTubeKit Tests/XCDYouTubeVideoTestCase.m
+++ XCDYouTubeKit Tests/XCDYouTubeVideoTestCase.m
@@ -12,54 +12,6 @@
 
 @implementation XCDYouTubeVideoTestCase
 
-- (void) testNilInfo
-{
-	NSError *error;
-	NSURL *url = [NSURL URLWithString:@"https://www.youtube.com/get_video_info"];
-	NSURLResponse *response = [[NSURLResponse alloc] initWithURL:url MIMEType:@"application/x-www-form-urlencoded" expectedContentLength:NSURLResponseUnknownLength textEncodingName:nil];
-	XCDYouTubeVideo *video = [[XCDYouTubeVideo alloc] initWithIdentifier:nil info:nil response:response error:&error];
-	XCTAssertNil(video);
-	XCTAssertEqualObjects(error.domain, XCDYouTubeVideoErrorDomain);
-	XCTAssertEqual(error.code, XCDYouTubeErrorNoStreamAvailable);
-	XCTAssertEqualObjects(url, error.userInfo[NSURLErrorKey]);
-}
-
-- (void) testEmptyInfo
-{
-	NSError *error;
-	NSURL *url = [NSURL URLWithString:@"https://www.youtube.com/get_video_info"];
-	NSURLResponse *response = [[NSURLResponse alloc] initWithURL:url MIMEType:@"application/x-www-form-urlencoded" expectedContentLength:NSURLResponseUnknownLength textEncodingName:nil];
-	XCDYouTubeVideo *video = [[XCDYouTubeVideo alloc] initWithIdentifier:nil info:@{} response:response error:&error];
-	XCTAssertNil(video);
-	XCTAssertEqualObjects(error.domain, XCDYouTubeVideoErrorDomain);
-	XCTAssertEqual(error.code, XCDYouTubeErrorNoStreamAvailable);
-	XCTAssertEqualObjects(url, error.userInfo[NSURLErrorKey]);
-}
-
-- (void) testInvalidInfo
-{
-	NSError *error;
-	NSURL *url = [NSURL URLWithString:@"https://www.youtube.com/get_video_info"];
-	NSURLResponse *response = [[NSURLResponse alloc] initWithURL:url MIMEType:@"application/x-www-form-urlencoded" expectedContentLength:NSURLResponseUnknownLength textEncodingName:nil];
-	XCDYouTubeVideo *video = [[XCDYouTubeVideo alloc] initWithIdentifier:nil info:@{ @"url_encoded_fmt_stream_map": @"" } response:response error:&error];
-	XCTAssertNil(video);
-	XCTAssertEqualObjects(error.domain, XCDYouTubeVideoErrorDomain);
-	XCTAssertEqual(error.code, XCDYouTubeErrorNoStreamAvailable);
-	XCTAssertEqualObjects(url, error.userInfo[NSURLErrorKey]);
-}
-
-- (void) testIncompleteInfo
-{
-	NSError *error;
-	NSURL *url = [NSURL URLWithString:@"https://www.youtube.com/get_video_info"];
-	NSURLResponse *response = [[NSURLResponse alloc] initWithURL:url MIMEType:@"application/x-www-form-urlencoded" expectedContentLength:NSURLResponseUnknownLength textEncodingName:nil];
-	XCDYouTubeVideo *video = [[XCDYouTubeVideo alloc] initWithIdentifier:nil info:@{ @"url_encoded_fmt_stream_map": @"url=" } response:response error:&error];
-	XCTAssertNil(video);
-	XCTAssertEqualObjects(error.domain, XCDYouTubeVideoErrorDomain);
-	XCTAssertEqual(error.code, XCDYouTubeErrorNoStreamAvailable);
-	XCTAssertEqualObjects(url, error.userInfo[NSURLErrorKey]);
-}
-
 - (void) testVideoEquality
 {
 	XCDYouTubeVideo *videoA = [[XCDYouTubeVideo alloc] initWithIdentifier:@"videoA" info:@{ @"url_encoded_fmt_stream_map": @"url=http://www.youtube.com/videoA.mp4&itag=123"} error:NULL];
